1ecf5f4cd121ac042eadcfce58d47b3609174ac7
mmn2310 <mmn2310@alose.enseeiht.fr>
2026-04-01 17:40:07 +0200

    src

---
+++ src/transfert_learning.py
@@ -0,0 +1,14 @@
+import PIL
+from PIL import Image
+import csv
+import numpy as np
+import matplotlib.pyplot as plt
+
+import tensorflow as tf
+
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras import models
+from tensorflow.keras.layers import Dense, Conv2D, MaxPooling2D, Flatten, Dropout, Input
+from tensorflow.keras.models import Model, Sequential
+from tensorflow.keras.optimizers import Adam
