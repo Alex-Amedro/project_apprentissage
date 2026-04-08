8802903b75cf14b6673c0c237a23ab689aac9372
mmn2310 <mmn2310@boeing.enseeiht.fr>
2026-04-08 10:36:22 +0200

    pour toi noé

--- src/transfert_learning.py
+++ src/transfert_learning.py
@@ -12,3 +12,13 @@ from tensorflow.keras import models
 from tensorflow.keras.layers import Dense, Conv2D, MaxPooling2D, Flatten, Dropout, Input
 from tensorflow.keras.models import Model, Sequential
 from tensorflow.keras.optimizers import Adam
+
+
+
+# Chargement d'un dataset TensorFlow à partir du dossier d'images
+train_dataset = tf.keras.utils.image_dataset_from_directory(
+    'train/images',
+    labels=None, # Mettre None si les labels sont dans un autre dossier
+    image_size=(224, 224), # Redimensionner vos images si besoin
+    batch_size=32
+)
